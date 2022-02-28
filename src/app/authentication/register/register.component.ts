40fd2b23028671212e3d4f1e70c7b2c179d31de5
julio <juliodbarriga@gmail.com>
2022-02-28 12:49:58 -0600

    User Registration working

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private appService: AppService, 
+    private formBuilder: FormBuilder) {
+      this.form = this.formBuilder.group({
+        phoneNumber: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(10)]],
+        userName: ['', Validators.required],
+        birthDate: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(6)]]
+      });
+     }
+
+  get f() { return this.form.controls; }
+
+  registerUser(): void {
+    this.submitted = true;
+
+    if (this.form.invalid){
+      return;
+    }
+
+    this.loading = true;
+    console.log(this.form.value);
+    this.appService.registerUser(this.form.value).subscribe(data => {
+      console.log(data);
+    })
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
---
+++ src/app/user.ts
@@ -0,0 +1,6 @@
+export class User {
+    id: number | undefined;
+    phoneNumber: string | undefined;
+    userName: string | undefined;
+    password: string | undefined;
+}
