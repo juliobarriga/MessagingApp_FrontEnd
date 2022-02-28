550e8a2df4cd3b3d29bcab213147a2714317d264
julio <juliodbarriga@gmail.com>
2022-02-27 20:45:06 -0600

    Deleted everything to install bootstrap and ease the front end development.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ConversationsPanelComponent } from './conversations-panel/conversations-panel.component';
-import { ConversationComponent } from './conversation/conversation.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConversationsPanelComponent,
-    ConversationComponent,
-    LoginComponent,
-    RegisterComponent
   ],
   imports: [
     BrowserModule,
