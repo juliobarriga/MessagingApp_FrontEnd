f1433806072dd3ecabb451d83e1c5d6c1ec6b0af
julio <juliodbarriga@gmail.com>
2022-03-02 14:58:22 -0600

    added cors config.

--- server.js
+++ server.js
@@ -6,8 +6,27 @@ function requireHTTPS(req, res, next) {
     next();
 }
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
 const app = express();
 app.use(requireHTTPS);
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+app.use(cors(corsOptions));
 
 app.use(express.static('./dist/messaging-app'));
 
