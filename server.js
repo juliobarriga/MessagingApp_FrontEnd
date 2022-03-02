3336c8b96542fe3159040d39240b324f7d0c943a
julio <juliodbarriga@gmail.com>
2022-03-02 15:22:20 -0600

    Followed this https://stackoverflow.com/questions/66509328/getting-application-error-code-503-favicon-angular.

--- server.js
+++ server.js
@@ -1,43 +1,57 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const app = express();
-app.use(requireHTTPS);
+  //Install express server
+  const express = require('express');
+  const path = require('path');
+  const app = express();
 
-const whitelist = ['https://messagingapp--api.herokuapp.com/']; // list of allow domain
+  // Serve only the static files form the dist directory
+  app.use(express.static(__dirname + '/dist/messaging-app'));
+  app.get('/*', function(req,res) {
+     res.sendFile(path.join(__dirname+'/dist/messaging-app/index.html'));
+  });
 
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) {
-            return callback(null, true);
-        }
+  // Start the app by listening on the default Heroku port
+  app.listen(process.env.PORT || 8080);
 
-        if (whitelist.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}
+// function requireHTTPS(req, res, next) {
+//     // The 'x-forwarded-proto' check is for Heroku
+//     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//         return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     next();
+// }
+// const express = require('express');
+// // const path = require('path');
+// const cors = require('cors');
+// const app = express();
+// // app.use(requireHTTPS);
 
-app.use(cors(corsOptions));
+// const whitelist = ['https://messagingapp--api.herokuapp.com/']; // list of allow domain
 
-app.use(express.static('./dist/messaging-app'));
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (!origin) {
+//             return callback(null, true);
+//         }
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/messaging-app/'}
-    );
-});
+//         if (whitelist.indexOf(origin) === -1) {
+//             var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//             return callback(new Error(msg), false);
+//         }
+//         return callback(null, true);
+//     }
+// }
 
-app.listen(process.env.PORT || 8080);
+// app.use(cors(corsOptions));
+
+// app.use(express.static('./dist/messaging-app'));
+
+// app.get('/*', function(req, res) {
+//     res.sendFile('index.html', {root: 'dist/messaging-app/'}
+//     );
+// });
+
+// app.listen(process.env.PORT || 8080);
 
 // //Install express server
 // const express = require('express');
