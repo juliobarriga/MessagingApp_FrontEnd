c6f39cf55226ac9ebb6c434e35af7d4b8db14265
julio <juliodbarriga@gmail.com>
2022-03-02 14:51:43 -0600

    Added config on package.json

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
-//Install express server
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
-
 const app = express();
+app.use(requireHTTPS);
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/messaging--app'));
+app.use(express.static('./dist/messaging-app'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/messaging--app/'}),
-);
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/messaging-app/'}
+    );
+});
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static('./dist/messaging-app'));
+
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/messaging-app/'}),
+// );
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
